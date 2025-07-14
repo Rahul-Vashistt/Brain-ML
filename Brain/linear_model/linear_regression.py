c5b60f942c4bf0a463eb4d1f24ce28e17a1412c2
Rahul-Vashistt <rahulvashist14102004@gmail.com>
2025-07-15 01:45:04 +0530

    Update Linear Regression

--- Brain/linear_model/linear_regression.py
+++ Brain/linear_model/linear_regression.py
@@ -6,26 +6,36 @@ from Brain.optimization import Batch_GD, Stochastic_GD, Mini_Batch_GD
 from Brain.method import optimizers
 
 class LinearRegression(BaseModel):
-    def __init__(self, method=None, learning_rate=None, epochs=1000, batch_size=None, n_batches=None):
+    def __init__(self):
         super().__init__()
-        self.method = method
-        self.learning_rate = learning_rate
-        self.epochs = epochs
-        self.batch_size = batch_size
-        self.n_batches = n_batches
 
     def fit(self,X,y):
+        """
+    Fit linear regression model.
+
+    Parameters
+    ----------
+    X : array-like
+        Features
+    y : array-like
+        Target
+
+    Returns
+    -------
+    self : object
+        Returns self.
+    """
         X,y = self.prep_data(X,y)
         method = resolve_optimizer(self.method)
 
         if method == 'ols':
             theta = np.pinv(X.T @ X) @ X.T @ y
         elif method == 'batch_gd':
-            theta = Batch_GD(X, y, self.learning_rate, self.epochs)
+            theta = Batch_GD(X, y, self.learning_rate, self.epochs, self.tol)
         elif method == 'sgd':
-            theta = Stochastic_GD(X, y, self.learning_rate, self.epochs)
+            theta = Stochastic_GD(X, y, self.learning_rate, self.epochs, self.tol)
         elif method == 'mbgd':
-            theta = Mini_Batch_GD(X, y, self.learning_rate, self.epochs, self.batch_size, self.n_batches)
+            theta = Mini_Batch_GD(X, y, self.learning_rate, self.epochs, self.batch_size, self.n_batches, self.tol)
 
         self.coef = theta[1:]
         self.intercept = theta[0]
